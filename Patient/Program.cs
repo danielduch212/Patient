5a696c7b5726fc578742f2cc72f98c9869fa9c9f
danielduch212 <danielduchnowicz1@gmail.com>
2024-08-08 19:01:41 +0200

    basic routing from logging panel + starting front but begining

--- Patient/Program.cs
+++ Patient/Program.cs
@@ -4,14 +4,9 @@ using Patient.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-
-
-
-
-
 
 var app = builder.Build();
 
@@ -26,6 +21,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
