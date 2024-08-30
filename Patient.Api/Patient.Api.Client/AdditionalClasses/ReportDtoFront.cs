5e5bb5cc88f02260ddc864ae58de56be7e8597d0
danielduch212 <danielduchnowicz1@gmail.com>
2024-08-30 14:12:41 +0200

    Adjusting

---
+++ Patient.Api/Patient.Api.Client/AdditionalClasses/ReportDtoFront.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Patient.Api.Client.AdditionalClasses;
+
+public class ReportDtoFront
+{
+    public string Description { get; set; } = default!;
+    public IEnumerable<Stream>? Files { get; set; } = new List<Stream>();
+    public bool IsChecked { get; set; } = false;
+
+
+
+
+    //
+
+}
