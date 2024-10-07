b0ed15368fb6398e23c721f11b2e280c96fbcc70
danielduch212 <danielduchnowicz1@gmail.com>
2024-10-08 00:50:00 +0200

    Test

--- Patient.Domain/Entities/Additional/Address.cs
+++ Patient.Domain/Entities/Additional/Address.cs
@@ -5,4 +5,5 @@ public class Address
     public string? City { get; set; }
     public string? Street { get; set; }
     public string? PostalCode { get; set; }
+    public string Country { get; set; } 
 }
